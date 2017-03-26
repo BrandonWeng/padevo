6f2948468042126bdf0d71930f4f2a11194fc223
Brandon Weng <wengbrandon@gmail.com>
2017-03-26 12:22:30 -0400

    added config files to be ignored and data script

--- data.js
+++ data.js
@@ -4,30 +4,59 @@
 
 
 var request = require('request');
+var fs = require('fs');
 
-var url = 'https://www.padherder.com/api/'
+var url = 'https://www.padherder.com/'
 
-function monsters(evolutions) {
-    request(url + 'monsters/', function (err, res, body) {
+function getUSmonsters(evolutions) {
+    request(url + 'api/monsters/', function (err, res, body) {
         if (err) console.log("ERROR ", err);
         var response = JSON.parse(body);
+
+        var NAmonsters = [];
 
         response.forEach(function (monster) {
             if (!monster.jp_only) {
-                console.log(monster.name + " " + monster.id);
-            }
-            ;
+                NAmonsters.push(monster);
+            };
         })
+
+        if (NAmonsters) getEvolutions(NAmonsters);
+        else console.log("ERROR no monsters found!")
     });
 }
 
-function evolutions() {
-    request(url + 'evolutions/', function (err, res, body) {
+function getEvolutions(NAmonsters) {
+
+    request(url + 'api/evolutions/', function (err, res, body) {
         if (err) console.log("ERROR ", err);
         var response = JSON.parse(body);
-        console.log(response['3003'][0])
+        store(response,NAmonsters);
+        console.log("Got evolutions")
     });
+
+
+}
+
+function store(evolutions,NAmonsters){
+
 }
 
 
+var download = function(uri, filename, callback){
+    var path = "./public/images/";
 
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+        request(uri).pipe(fs.createWriteStream(path+filename)).on('close', callback);
+    });
+};
+
+/*
+download( url + '/static/img/monsters/40x40/1087.86c7840800d0.png', 'google.png', function(){
+    console.log('done');
+});
+*/
+
+getUSmonsters(getEvolutions)
