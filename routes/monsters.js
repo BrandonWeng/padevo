d6cae3ea470fe1fb1c235a512bdcfc454b95da09
Brandon Weng <wengbrandon@gmail.com>
2017-03-29 11:28:53 -0400

    accepts get requests from /monsters/:id

---
+++ routes/monsters.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var Monsters = require('../model/monstersModel')
+
+/* GETS MONSTER ID */
+router.get('/:id',function(req,res,next){
+    res.render('index', { title: req.params.id });
+});
+
+module.exports = router;
