320b761684b917d1c58a10ca41aad7f6d18e3599
Brandon Weng <wengbrandon@gmail.com>
2017-05-14 00:29:09 -0400

    added a post request router to monsters

--- routes/monsters.js
+++ routes/monsters.js
@@ -8,13 +8,22 @@ var db = require('../model/monstersModel')
 
 /* GETS MONSTER ID */
 router.get('/:id',function(req,res,next){
-    console.log(req.body)
+    console.log("REQUEST" + req);
     db.findOne({id:req.params.id},function (err,docs){
         if (err) throw err;
-        else console.log(docs);
-        res.render('monster.jade', { title: docs.name, monster:docs.materials });
+        else res.render('monster.jade', { title: docs.name, monster:docs.materials });
     });
 
 });
+
+router.post('/',function(req,res,next){
+    var id = parseInt(req.body.monster_id);
+    db.findOne({id:id},function(err,docs){
+        console.log(docs,req.body,id)
+        if (err) throw err;
+        else res.render('monster',{title:docs.name,monster:docs.materials})
+    });
+});
+
 
 module.exports = router;
