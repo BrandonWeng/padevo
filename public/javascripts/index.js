7142f98bc48cdbc8c51d9ab166f81d5ca7387fe7
Brandon Weng <wengbrandon@gmail.com>
2017-05-14 20:00:22 -0400

    mobile images resize properly

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,10 @@
 $('.evo_material').click(function(){
   $(this).toggleClass('blur');
 });
+
+$(window).on('resize',function(){
+  if ($(window.innerWidth) < 768){
+    $('.mat').removeClass('col-sm-1');
+    $('.mat').addClass('col-xs-3')
+  }
+})
